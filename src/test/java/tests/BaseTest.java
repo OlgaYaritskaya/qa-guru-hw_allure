57913442d79266c8bb7ac5fdf6eecd46d6766c31
OlgaYaritskaya <olga.yaritskaya@yandex.ru>
2023-12-24 21:02:34 +0300

    added BaseTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,57 +2,22 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.util.Map;
 
 public class BaseTest {
-package tests;
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://www.github.com";
+        Configuration.holdBrowserOpen = true;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import helpers.Attach;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.remote.DesiredCapabilities;
+    }
 
-import java.util.Map;
-
-import static com.codeborne.selenide.Selenide.*;
-
-    public class BaseTest {
-        @BeforeAll
-        public static void beforeAll() {
-            Configuration.browserSize = "1920x1080";
-            Configuration.pageLoadStrategy = "eager";
-            Configuration.baseUrl = "https://sport-marafon.ru";
-            //Configuration.holdBrowserOpen = true;
-            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-            SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("selenoid:options",  Map.<String, Object>of(
-                    "enableVNC", true,
-                    "enableVideo", true
-            ));
-            Configuration.browserCapabilities = capabilities;
-
-        }
-
-        @AfterEach
-        void addAttachments() {
-            Attach.screenshotAs("Last screenshot");
-            Attach.pageSource();
-            Attach.browserConsoleLogs();
-            Attach.addVideo();
-            Selenide.closeWebDriver();
-        }
+    @AfterEach
+    void afterEach(){
+        Selenide.closeWebDriver();
     }
 
 }
