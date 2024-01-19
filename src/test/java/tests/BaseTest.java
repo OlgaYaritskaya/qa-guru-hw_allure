c34ccb5c5af42afbfefae2be3f3704b61982a245
OlgaYaritskaya <olga.yaritskaya@yandex.ru>
2024-01-19 18:16:19 +0300

    added Listener

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,6 +2,8 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -10,6 +12,7 @@ public class BaseTest {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        SelenideLogger.addListener("allure", new AllureSelenide()); //добавляет лог по шагам
         // Configuration.holdBrowserOpen = true;
     }
 
